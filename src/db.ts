e10f2ac72da8778132a664a8fb6de668b8b34a88
Emmanuel Jonathan <emmanueljonathan113@gmail.com>
2026-09-14 18:54:30 +0100

    update my prisma and prisma configuration to align with my environmental variable

--- src/db.ts
+++ src/db.ts
@@ -1,24 +1,16 @@
 import { readReplicas } from "@prisma/extension-read-replicas";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "./generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 const mainAdapter = new PrismaPg(pool);
-
-const mianClient = new PrismaClient({ adapter: mainAdapter });
+const mainClient = new PrismaClient({ adapter: mainAdapter });
 
 const replicaPool1 = new Pool({ connectionString: process.env.REPLICA_URL1 });
 const replicaAdapter1 = new PrismaPg(replicaPool1);
-
 const replicaClient1 = new PrismaClient({ adapter: replicaAdapter1 });
 
-const replicaPool2 = new Pool({ connectionString: process.env.REPLICA_URL2 });
-const replicaAdapter2 = new PrismaPg(replicaPool2);
-
-const replicaClient2 = new PrismaClient({ adapter: replicaAdapter2 });
-export const prisma = mianClient.$extends(readReplicas({
-    replicas: [replicaClient1, replicaClient2]
-}))
-
-
+export const prisma = mainClient.$extends(readReplicas({
+  replicas: [replicaClient1], // only reference replicas that actually exist
+}));
